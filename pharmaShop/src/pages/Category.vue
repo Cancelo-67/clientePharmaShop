775ae9fb8dc6cec6f12c57945f7023c3ef34ba0f
Cancelo-67 <alejandro.cancelo@iesrafaelalberti.es>
2023-12-13 13:27:53 +0100

    Cambios productos y categorias

--- pharmaShop/src/pages/Category.vue
+++ pharmaShop/src/pages/Category.vue
@@ -3,7 +3,37 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      category: null,
+      products: [],
+    };
+  },
+  methods: {
+    async fetchProducts() {
+      const userToken = JSON.parse(
+        decodeURIComponent(Cookies.get("userToken"))
+      );
+      console.log("prueba");
+
+      try {
+        const currentRoute = this.$route;
+        this.category = currentRoute.params.category;
+        console.log(this.category);
+        const response = await axios.get("http://localhost:8080/products/", {
+          headers: {
+            Authorization: `Bearer ${userToken}`,
+          },
+        });
+
+        this.products = response.data;
+      } catch (error) {
+        console.error("Error al obtener los productos:", error);
+      }
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
